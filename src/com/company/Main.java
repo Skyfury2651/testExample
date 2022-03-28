6230ced5f15df5f82cd340cbea3035fbf8ed85fe
HungNB <hung.nguyen@pirago.work>
2022-03-28 21:31:08 +0700

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance();
+        singleton.addPlayer();
+
+        Singleton singleton1 = Singleton.getInstance();
+        singleton1.addPlayer();
+
+        System.out.println(singleton.getCurrentPlayer());
+        System.out.println(singleton1.getCurrentPlayer());
+    }
+}
